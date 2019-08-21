4d0c8b3a7f23ded7dd1fccb3cef0f7314c0e8332
Tramy Nguyen <tramynn2@gmail.com>
2019-08-21 17:55:26 -0500

    turned Header component to function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 
-class Header extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-
-        };
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Home</h1>
-            </div>
-        );
-    }
+function Header() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
 }
 
 export default Header;
