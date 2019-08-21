31a48b50187d2720b5b46bc06e0456c4eca213b6
Tramy Nguyen <tramynn2@gmail.com>
2019-08-21 14:12:12 -0500

    added sections to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 //components
 
 //css
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App-main-container">
+        <header className="App-header">
+          <section className="App-home-nav">
+          </section>
+        </header>
+        <main className="App-body">
+          <section className="App-text-box">
+          </section>
+          <section className="App-text-box-nav">
+            <section className="App-previous-nav">
+              
+            </section>
+            <section className="App-edit-delete-new">
+
+            </section>
+            <section className="App-next-nav">
+
+            </section>
+          </section>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
